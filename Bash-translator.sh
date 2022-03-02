026bf2f12895069046efd8a3668a2f75e2e3336c
user <user@example.com>
2022-03-02 16:38:56 +0200

    update

--- Bash-translator.sh
+++ Bash-translator.sh
@@ -14,15 +14,15 @@ word_HEX=$(printf $var_word| xxd -p -u -i| sed 's/ 0X/%/g; s/%0A//; s/,//g'| tr 
 # With File
 curl -s https://bg.glosbe.com/bg/en/$word_HEX > $HOME/.Bash-Translator-temp
 
-printf "\nResulte\n\n"
+printf "\nResult/Резултат\n\n"
 
 # With sed command
-grep -m 3 'data-translation=' $HOME/.fast_Words_Temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d; s/"//g'
+grep -m 3 'data-translation=' $HOME/.Bash-Translator-temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d; s/"//g'
 
 # With tr command
-#grep -m 3 'data-translation=' $HOME/.fast_Words_Temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d'| tr -d /\"/
+#grep -m 3 'data-translation=' $HOME/.Bash-Translator-temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d'| tr -d /\"/
 
-printf "\nExamples\n"
+printf "\nExamples/Примери\n"
 #printf "|--------------------|"
 
 grep -A 1 -m 3 '<p >' $HOME/.Bash-Translator-temp | sed 's|<p class="ml-4 " lang="en" >|EN - |; s|<p >|BG - |; s|<strong>||; s|</strong>||; s|</p>||; s|&#39;|`|'

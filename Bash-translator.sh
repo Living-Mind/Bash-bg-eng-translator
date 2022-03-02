fa14447d2182a85dc09ce78a99c35cfa91c74e6c
user <user@example.com>
2022-03-02 15:00:03 +0200

    update

--- Bash-translator.sh
+++ Bash-translator.sh
@@ -14,18 +14,20 @@ word_HEX=$(printf $var_word| xxd -p -u -i| sed 's/ 0X/%/g; s/%0A//; s/,//g'| tr 
 # With File
 curl -s https://bg.glosbe.com/bg/en/$word_HEX > $HOME/.Bash-Translator-temp
 
-printf "\nResulte\n"
-grep 'data-translation=' $HOME/.fast_Words_Temp
+printf "\nResulte\n\n"
+
+# With sed command
+grep -m 3 'data-translation=' $HOME/.fast_Words_Temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d; s/"//g'
+
+# With tr command
+#grep -m 3 'data-translation=' $HOME/.fast_Words_Temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d'| tr -d /\"/
 
 printf "\nExamples\n"
 #printf "|--------------------|"
 
-grep '<p >' $HOME/.Bash-Translator-temp
+grep -A 1 -m 3 '<p >' $HOME/.Bash-Translator-temp | sed 's|<p class="ml-4 " lang="en" >|EN - |; s|<p >|BG - |; s|<strong>||; s|</strong>||; s|</p>||; s|&#39;|`|'
 
 #if RegEx for bg or eng
 
-
 printf "\n\nSearch for new word:\n\n"
-#echo $HEX
-
 

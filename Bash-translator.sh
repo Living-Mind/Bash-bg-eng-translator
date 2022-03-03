70e8471b45ec63555b78efea22e69d103bc4eead
user <user@example.com>
2022-03-03 20:11:46 +0200

    update function

--- Bash-translator.sh
+++ Bash-translator.sh
@@ -2,32 +2,73 @@
 
 printf "\nWelcome to Bash-Translator\n"
 
-printf "\nSearch for a word:\n"
+printf "\nType a word:\n"
 
-read var_word
+function main(){
+	read var_word
+	
+	word_HEX=$(printf $var_word| xxd -p -u -i| sed 's/ 0X/%/g; s/%0A//; s/,//g'| tr -d "[:space:]")
 
-word_HEX=$(printf $var_word| xxd -p -u -i| sed 's/ 0X/%/g; s/%0A//; s/,//g'| tr -d "[:space:]")
+	regEx=
 
-# Without File
-#curl -s https://bg.glosbe.com/bg/en/$word_HEX| grep '<p >'
+	tildaP=~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# With File
-curl -s https://bg.glosbe.com/bg/en/$word_HEX > $HOME/.Bash-Translator-temp
+	resultP=Result\|Резултат
 
-printf "\nResult/Резултат\n\n"
+	egP=Examples\|Примери
 
-# With sed command
-grep -m 3 'data-translation=' $HOME/.Bash-Translator-temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d; s/"//g'
+	if [ $var_word = q ]; 
+	then
+		exit
 
-# With tr command
-#grep -m 3 'data-translation=' $HOME/.Bash-Translator-temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d'| tr -d /\"/
+	elif [[ $var_word = [A-z]* ]];
+	then
+		echo "ENG"
 
-printf "\nExamples/Примери\n"
-#printf "|--------------------|"
+		printf "\n $resultP\n"
+		
+		# With sed command
+		grep -m 3 'data-translation=' $HOME/.Bash-Translator-temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d; s/"//g'
+		printf "\n $egP\n"
+		
+		grep -A 1 -m 3 '<p >' $HOME/.Bash-Translator-temp | sed 's|<p class="ml-4 " lang="en" >|EN - |; s|<p >|BG - |; s|<strong>||; s|</strong>||; s|</p>||; s|&#39;|`|g'
 
-grep -A 1 -m 3 '<p >' $HOME/.Bash-Translator-temp | sed 's|<p class="ml-4 " lang="en" >|EN - |; s|<p >|BG - |; s|<strong>||; s|</strong>||; s|</p>||; s|&#39;|`|'
+		printf $tildaP
+
+
+	elif [[ $var_word != [A-z]* ]];
+	then
+
+		printf "\n $resultP\n"
+		
+		# With sed command
+		grep -m 3 'data-translation=' $HOME/.Bash-Translator-temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d; s/"//g'
+		
+		# With tr command
+		#grep -m 3 'data-translation=' $HOME/.Bash-Translator-temp| grep -o -E '"[a-z]*"'| sed '/"phrase"/d'| tr -d /\"/
+
+		printf "\n $egP\n"
+		
+		grep -A 1 -m 3 '<p >' $HOME/.Bash-Translator-temp | sed 's|<p class="ml-4 " lang="en" >|EN - |; s|<p >|BG - |; s|<strong>||; s|</strong>||; s|</p>||; s|&#39;|`|g'
+
+		printf $tildaP
+
+	fi
+	
+	# Without File
+	#curl -s https://bg.glosbe.com/bg/en/$word_HEX| grep '<p >'
+	
+	# With File
+	curl -s https://bg.glosbe.com/bg/en/$word_HEX > $HOME/.Bash-Translator-temp
+
+	printf "\n\nType a new word or type exit:\n"
+	main
+}
+
+main
+
 
 #if RegEx for bg or eng
 
-printf "\n\nSearch for new word:\n\n"
+#tput clear
 
